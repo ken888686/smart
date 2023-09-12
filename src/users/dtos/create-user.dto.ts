15261d8f2d7899b3791bda651b25ab7a9fe44cad
YungChun Tu <ken888686@gmail.com>
2023-09-12 18:15:47 +0900

    add validation

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,6 +1,10 @@
+import { IsNotEmpty } from 'class-validator';
 import { Status } from '../user.model';
 
 export class CreateUserDto {
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   status: Status;
 }
